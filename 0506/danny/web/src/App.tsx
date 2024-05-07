3a771d407db1c8f9a8840e193e752502ff2800e8
老房东 <huangdong@gmail.com>
2024-05-07 07:16:30 -0400

    use ant search box replease customInput

--- 0506/danny/web/src/App.tsx
+++ 0506/danny/web/src/App.tsx
@@ -3,7 +3,8 @@ import { Refine } from "@refinedev/core";
 import dataProvider from "@refinedev/simple-rest";
 import { ConfigProvider, App as AntdApp, Button } from "antd";
 import { DoctorTable } from "./doctor-table";
-import CustomInput from "./components/CustomInput";
+// import CustomInput from "./components/CustomInput";
+import { Select } from "antd";
 
 const DEFAULT_API_URL =
   "https://hdcola.github.io/FSD12Practice/0505/danny/web/dist/";
@@ -21,10 +22,27 @@ export default function App() {
       <AntdApp>
         <div style={{ display: "flex", alignItems: "center" }}>
           <label style={{ marginRight: "10px" }}>API URL:</label>
-          <CustomInput
+          <Select
+            showSearch
+            placeholder="Select API URL"
+            optionFilterProp="children"
+            onChange={(value) => setApiUrl(value)}
+            onSearch={(value) => setApiUrl(value)}
+            options={[
+              {
+                value: DEFAULT_API_URL,
+                label: DEFAULT_API_URL,
+              },
+              {
+                value: "http://localhost:8080/api",
+                label: "http://localhost:8080/api",
+              },
+            ]}
+          />
+          {/* <CustomInput
             options={[DEFAULT_API_URL, "http://localhost:8080/api"]}
             onInputChange={(value: string) => setApiUrl(value)}
-          />
+          /> */}
           <Button onClick={handleRefresh}>Refresh</Button>
         </div>
         <Refine key={refreshKey} dataProvider={dataProvider(apiUrl)}>
