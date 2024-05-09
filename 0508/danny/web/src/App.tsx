8b7ea8d2997f27452db84d33033a2b78aac2459e
老房东 <huangdong@gmail.com>
2024-05-08 21:49:21 -0400

    call api and show response

--- 0508/danny/web/src/App.tsx
+++ 0508/danny/web/src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "react-bootstrap";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const DEFAULT_URL = "http://localhost:8080/api/doctors";
+  const [data, setData] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  async function fetchData() {
+    console.log("Fetching data...");
+    setIsLoading(true);
+    try {
+      const response = await fetch(DEFAULT_URL);
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      )}
+      <Button variant="primary" onClick={fetchData}>
+        Fetch Data
+      </Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
