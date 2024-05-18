0651de5587feb66864209d2a8dd1967648a66737
Allenyzh <allenyangzhenhao@gmail.com>
2024-05-18 14:32:06 -0400

    0518 update

--- 0518/allen/webPracticeGrid/src/App.tsx
+++ 0518/allen/webPracticeGrid/src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap-grid.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container text-center">
+      <div className="row justify-content-md-center">
+        <div className="col col-lg-2">1 of 3</div>
+        <div className="col-md-auto">Variable width content</div>
+        <div className="col col-lg-2">3 of 3</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="row">
+        <div className="col">1 of 3</div>
+        <div className="col-md-auto">Variable width content</div>
+        <div className="col col-lg-2">3 of 3</div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
