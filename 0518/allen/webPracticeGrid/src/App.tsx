0eb4f0f9e08a78137d7fa0b4ab8c3821b981768b
Allenyzh <allenyangzhenhao@gmail.com>
2024-05-19 01:55:12 -0400

    about & experience completed

--- 0518/allen/webPracticeGrid/src/App.tsx
+++ 0518/allen/webPracticeGrid/src/App.tsx
@@ -1,20 +1,107 @@
 // import 'bootstrap/dist/css/bootstrap.css';
 // import 'bootstrap/dist/css/bootstrap-grid.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="container text-center">
-      <div className="row justify-content-md-center">
-        <div className="col col-lg-2">1 of 3</div>
-        <div className="col-md-auto">Variable width content</div>
-        <div className="col col-lg-2">3 of 3</div>
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container" id="first-row">
+          <div className="row">
+            <h1 className="navbar-brand" id="name" href="#">
+              Zhenhao Yang
+            </h1>
+          </div>
+          <div className="row">
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link" href="#About">
+                    About
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#Experience">
+                    Experience
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#Skills">
+                    Skills
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#Education">
+                    Education
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <div className="container">
+        <div className="row" id="row1">
+          <div className="col-12" id="About">
+            About
+          </div>
+          <div className="col" id="about-content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+        </div>
       </div>
-      <div className="row">
-        <div className="col">1 of 3</div>
-        <div className="col-md-auto">Variable width content</div>
-        <div className="col col-lg-2">3 of 3</div>
+
+      <div className="container">
+        <div className="row" id="row2">
+          <div className="col-12" id="Experience">
+            Experience
+          </div>
+          <div className="row" id="experience-content">
+            <ul className="col-2 jd">
+              <li className="des">Company Name:</li>
+              <li className="des">Location:</li>
+              <li className="des">Duration:</li>
+              <li className="des">Job Title:</li>
+            </ul>
+            <div className="col-2 c">
+              <li className="des">Provigo</li>
+              <li className="des">Kirkland</li>
+              <li className="des">2022-Present</li>
+              <li className="des">Clerk</li>
+            </div>
+
+            <div className="col-2 jd text-wrap">Job Description</div>
+            <div className="col-6">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
