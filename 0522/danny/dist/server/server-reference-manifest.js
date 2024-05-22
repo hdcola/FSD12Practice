36cacafd4f93052a5329bfe5b4afc7894fbd4506
老房东 <huangdong@gmail.com>
2024-05-22 13:13:55 -0400

    0522 init

---
+++ 0522/danny/dist/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"FF97yxf9AYB/Dni3GnmEJ6RpfrINsjUh3l6Hyg1yLWI=\"\n}"
---
+++ 0522/danny/src/app/components/Message.js
@@ -0,0 +1,22 @@
+function Message({ role, content }) {
+  return (
+    <div
+      className={
+        role === "assistant"
+          ? "d-flex flex-row ai-chat-box"
+          : "d-flex flex-row user-chat-box"
+      }
+    >
+      <div className="chat-icon">
+        {role === "assistant" ? (
+          <i class="bi bi-robot"></i>
+        ) : (
+          <i class="bi bi-person-circle"></i>
+        )}
+      </div>
+      <div className="chat-text">{content}</div>
+    </div>
+  );
+}
+
+export default Message;
