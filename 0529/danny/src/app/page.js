8b67c390bfad7b96fa3746c7750d450b532be2d4
老房东 <huangdong@gmail.com>
2024-05-29 23:14:45 -0400

    finish

--- 0529/danny/src/app/page.js
+++ 0529/danny/src/app/page.js
@@ -1,21 +1,41 @@
+"use client";
 import Card from "./components/Card";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [scenarios, setScenarios] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const url =
+    "https://hdcola.github.io/FSD12Practice/0513/danny/web/build/scenarios";
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setScenarios(data.scenarios);
+        setLoading(false);
+      });
+  }, [scenarios, loading]);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-10">
       <h1 className="text-4xl font-bold">Scenarios</h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
-        <Card
-          title="Noteworthy technology acquisitions 2021"
-          description="Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order."
-        />
-        <Card
-          title="Noteworthy technology acquisitions 2021"
-          description="Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order."
-        />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          scenarios.map((scenario) => (
+            <Card
+              key={scenario.id}
+              title={scenario.name}
+              description={scenario.description}
+            />
+          ))
+        )}
       </div>
-      <p className="text-lg">Welecome to talk with AI in a lot of scenarios.</p>
+      <p className="text-lg py-8">
+        Welecome to talk with AI in a lot of scenarios.
+      </p>
     </main>
   );
 }
