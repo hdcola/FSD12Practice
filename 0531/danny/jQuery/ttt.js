51870d87c649af1192f691163498f160dc8ce5f0
老房东 <huangdong@gmail.com>
2024-06-04 07:50:59 -0400

    finished

--- 0531/danny/jQuery/ttt.js
+++ 0531/danny/jQuery/ttt.js
@@ -1,6 +1,8 @@
 var board = ["", "", "", "", "", "", "", "", ""];
 var currentPlayer = "X";
+var currentMove = 0;
 var gameActive = true;
+var historyInformation = Array(9).fill(null);
 
 var winningConditions = [
   [0, 1, 2],
@@ -30,7 +32,12 @@ function handleCellClick() {
   }
 
   board[index] = currentPlayer;
+  for (let i = index + 1; i < board.length; i++) {
+    board[i] = "";
+  }
   $(this).text(currentPlayer).addClass("disabled");
+  currentMove++;
+  historyInformation[currentMove] = board.slice();
 
   renderInformation();
 
@@ -49,16 +56,63 @@ function handleCellClick() {
   $("#message").text("Next Player: " + currentPlayer);
 }
 
-function renderInformation() {}
+function resetGame() {
+  board = ["", "", "", "", "", "", "", "", ""];
+  currentPlayer = "X";
+  currentMove = 0;
+  gameActive = true;
+  historyInformation = Array(9).fill(null);
+  historyInformation[0] = board.slice();
+  $(".cell").text("").removeClass("disabled");
+  $("#message").text("Next Player: " + currentPlayer);
+  renderInformation();
+}
+
+function renderInformation() {
+  $("#history").empty();
+  for (let index = 0; index < historyInformation.length; index++) {
+    let description;
+    if (historyInformation[index] === null) {
+      break;
+    }
+    if (index > 0) {
+      description = "Go to move #" + index;
+    } else {
+      description = "Go to game start";
+    }
+    $("#history").append(
+      "<li><button class='btn btn-sm m-1' data-index='" +
+        index +
+        "'>" +
+        description +
+        "</button></li>"
+    );
+    $("#history button").on("click", handleHistoryClick);
+  }
+}
+
+function handleHistoryClick() {
+  var index = $(this).data("index");
+  board = historyInformation[index].slice();
+  currentPlayer = index % 2 === 0 ? "X" : "O";
+  currentMove = index;
+  gameActive = true;
+  $(".cell").each(function (index, cell) {
+    $(cell).text(board[index] === "" ? "" : board[index]);
+    $(cell).toggleClass("disabled", board[index] !== "");
+    console.log(index, board[index]);
+  });
+  renderInformation();
+}
 
 $(document).ready(function () {
   for (let i = 0; i < 9; i++) {
     $("#board").append(
-      "<div class='cell bg-gray-100 aspect-square flex items-center justify-center cursor-pointer text-2xl disabled:bg-gray-200 disabled:cursor-not-allowed' data-index='" +
+      "<div class='cell bg-gray-100 aspect-square flex items-center justify-center enabled:cursor-pointer text-2xl' data-index='" +
         i +
         "' ></div>"
     );
   }
   $(".cell").on("click", handleCellClick);
-  $("#message").text("Next Player: " + currentPlayer);
+  resetGame();
 });
