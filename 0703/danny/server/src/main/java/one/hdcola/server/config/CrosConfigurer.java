70c9a5d122466c02f6dc220004557ca039dd1919
老房东 <huangdong@gmail.com>
2024-07-04 10:09:54 -0400

    cros domain support

--- 0703/danny/server/src/main/java/one/hdcola/server/config/CrosConfigurer.java
+++ 0703/danny/server/src/main/java/one/hdcola/server/config/CrosConfigurer.java
@@ -4,7 +4,6 @@ package one.hdcola.server.config;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
