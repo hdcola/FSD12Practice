c722916a170c950fa0d50ed57299dae5656ace68
老房东 <huangdong@gmail.com>
2024-09-06 10:51:34 -0400

    htmlx worked

--- 0905/danny/enroll/src/main/java/org/hdcola/enroll/Controllers/HomeController.java
+++ 0905/danny/enroll/src/main/java/org/hdcola/enroll/Controllers/HomeController.java
@@ -1,6 +1,9 @@
 package org.hdcola.enroll.Controllers;
 
+import io.github.wimdeblauwe.htmx.spring.boot.mvc.HtmxResponse;
+import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -9,6 +12,18 @@ public class HomeController {
 
     @GetMapping("/")
     public ModelAndView home() {
-        return new ModelAndView("home");
+        ModelAndView modelAndView =  new ModelAndView("home");
+        modelAndView.addObject("message", "Learn how to develop up-to-date websites and apps and get them out there for everyone to use.");
+        return modelAndView;
     }
+
+    @HxRequest
+    @GetMapping("/test")
+    public HtmxResponse test(Model model) {
+        model.addAttribute("message", "Hello, World!");
+        return HtmxResponse.builder()
+                .view("home :: test")
+                .build();
+    }
+
 }
