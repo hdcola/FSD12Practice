8780fa618525a2b4bbd06b5ba11075f83f4b8f78
老房东 <huangdong@gmail.com>
2024-09-10 08:18:55 -0400

    article edit

--- 0907/danny/blog/src/main/java/org/hdcola/blog/Controllers/ArticleController.java
+++ 0907/danny/blog/src/main/java/org/hdcola/blog/Controllers/ArticleController.java
@@ -1,5 +1,6 @@
 package org.hdcola.blog.Controllers;
 
+import org.springframework.transaction.annotation.Transactional;
 import org.hdcola.blog.Configs.CustomUserDetails;
 import org.hdcola.blog.Entities.Article;
 import org.hdcola.blog.Entities.User;
@@ -7,9 +8,9 @@ import org.hdcola.blog.Repositories.ArticleRepository;
 import org.hdcola.blog.Repositories.UserRepository;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -44,5 +45,25 @@ public class ArticleController {
         User user = UserRepository.findByEmail(authentication.getName());
         model.addAttribute("articles", articleRepository.findByUserId(user.getId()));
         return "article/list";
+    }
+
+    @GetMapping("/article/edit/{id}")
+    public String edit(@PathVariable("id") Long id, Model model) {
+        Article article = articleRepository.findById(id).get();
+        model.addAttribute("article", article);
+        return "article/new";
+    }
+
+    @PostMapping("/article/edit/{id}")
+    public String update(@PathVariable("id") Long id, Article article) {
+        article.setId(id);
+        articleRepository.save(article);
+        return "redirect:/article/list";
+    }
+
+    @GetMapping("/article/delete/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        articleRepository.deleteById(id);
+        return "redirect:/article/list";
     }
 }
