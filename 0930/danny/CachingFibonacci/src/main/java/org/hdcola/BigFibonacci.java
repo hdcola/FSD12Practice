f93e8bc2a0ced8dece06f5d1e93d356fe332072b
老房东 <huangdong@gmail.com>
2024-10-01 10:21:58 -0400

    add BigFibonacci use BigInteger

--- 0930/danny/CachingFibonacci/src/main/java/org/hdcola/BigFibonacci.java
+++ 0930/danny/CachingFibonacci/src/main/java/org/hdcola/BigFibonacci.java
@@ -1,23 +1,24 @@
 package org.hdcola;
 
+import java.math.BigInteger;
 import java.util.HashMap;
 
-class Fibonacci {
+public class BigFibonacci {
 
     private boolean isCacheOn;
 
-    Fibonacci(boolean cacheOn) {
+    BigFibonacci(boolean cacheOn) {
         isCacheOn = cacheOn;
-        fibsCached.put(0,0L); // #0
-        fibsCached.put(1,1L); // #1
+        fibsCached.put(0,BigInteger.valueOf(0)); // #0
+        fibsCached.put(1,BigInteger.valueOf(1)); // #1
     }
 
-    private HashMap<Integer,Long> fibsCached = new HashMap<>();
+    private HashMap<Integer,BigInteger> fibsCached = new HashMap<>();
     private int fibsCompCount = 2; // may not be needed
     // in a correct caching implementation fibsCompCount will end up the same as fibsCached.size();
 
     // when you implement caching, make this method print out the number of fib values it had to compute (as opposed to take from cache)
-    public long getNthFib(int n) {
+    public BigInteger getNthFib(int n) {
         if (isCacheOn) {
             if (fibsCached.containsKey(n)) {
                 return fibsCached.get(n);
@@ -32,15 +33,15 @@ class Fibonacci {
     // You can find implementation online, recursive or non-recursive.
     // For 100% solution you should use values in fibsCached as a starting point
     // instead of always starting from the first two values of 0, 1.
-    private long computeNthFib(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
+    private BigInteger computeNthFib(int n) {
+        if (n == 0) return BigInteger.valueOf(0);
+        if (n == 1) return BigInteger.valueOf(1);
         int lastCached = fibsCached.size() - 1;
-        long a = fibsCached.get(lastCached - 1);
-        long b = fibsCached.get(lastCached);
-        long c = 0;
+        BigInteger a = fibsCached.get(lastCached - 1);
+        BigInteger b = fibsCached.get(lastCached);
+        BigInteger c = BigInteger.valueOf(0);
         for (int i = lastCached + 1; i <= n; i++) {
-            c = a + b;
+            c = a.add(b);
             a = b;
             b = c;
             fibsCached.put(i, c);
--- 0930/danny/CachingFibonacci/src/main/java/org/hdcola/Main.java
+++ 0930/danny/CachingFibonacci/src/main/java/org/hdcola/Main.java
@@ -3,9 +3,17 @@ package org.hdcola;
 public class Main {
     public static void main(String[] args) {
         Fibonacci fib = new Fibonacci(true);
-        System.out.println(fib.getNthFib(5));
+        System.out.println("5th is: "+fib.getNthFib(5));
         System.out.println(fib.toString());
-        System.out.println(fib.getNthFib(10));
+        System.out.println("10th is: "+fib.getNthFib(10));
         System.out.println(fib.toString());
+        System.out.println("fibsCompCount is: " + fib.getCountOfFibsComputed());
+        System.out.println("====================================");
+        BigFibonacci bigFib = new BigFibonacci(true);
+        System.out.println("100th is: "+bigFib.getNthFib(100));
+        System.out.println(bigFib.toString());
+        System.out.println("100th is: " + bigFib.getNthFib(105));
+        System.out.println(bigFib.toString());
+        System.out.println("fibsCompCount is: " + bigFib.getCountOfFibsComputed());
     }
 }
