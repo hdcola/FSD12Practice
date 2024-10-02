51e420da4b7f6c8f2d5e2b2825e59f06e9e6a787
老房东 <huangdong@gmail.com>
2024-10-02 09:37:08 -0400

    update for check result

--- 0930/danny/CachingFibonacci/src/main/java/org/hdcola/Main.java
+++ 0930/danny/CachingFibonacci/src/main/java/org/hdcola/Main.java
@@ -3,16 +3,18 @@ package org.hdcola;
 public class Main {
     public static void main(String[] args) {
         Fibonacci fib = new Fibonacci(true);
-        System.out.println("5th is: "+fib.getNthFib(5));
-        System.out.println(fib.toString());
         System.out.println("10th is: "+fib.getNthFib(10));
+        System.out.println(fib.toString());
+        System.out.println("fibsCompCount is: " + fib.getCountOfFibsComputed());
+        System.out.println("20th is: "+fib.getNthFib(20));
         System.out.println(fib.toString());
         System.out.println("fibsCompCount is: " + fib.getCountOfFibsComputed());
         System.out.println("====================================");
         BigFibonacci bigFib = new BigFibonacci(true);
         System.out.println("100th is: "+bigFib.getNthFib(100));
         System.out.println(bigFib.toString());
-        System.out.println("100th is: " + bigFib.getNthFib(105));
+        System.out.println("fibsCompCount is: " + bigFib.getCountOfFibsComputed());
+        System.out.println("20th is: " + bigFib.getNthFib(20));
         System.out.println(bigFib.toString());
         System.out.println("fibsCompCount is: " + bigFib.getCountOfFibsComputed());
     }
