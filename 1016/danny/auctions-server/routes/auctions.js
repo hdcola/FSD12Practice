3f77662c7932c7220655c8bd43cf86181184df1c
hdcola <gh@hdcola.org>
2024-10-16 12:21:17 -0400

    create an auction and get all or by id

--- 1016/danny/auctions-server/routes/auctions.js
+++ 1016/danny/auctions-server/routes/auctions.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const { Auctions } = require('../models');
 
 /* GET auctions listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function (req, res, next) {
+  const auctions = await Auctions.findAll();
+  res.json(auctions).end();
 });
 
-router.get('/:id', function (req, res, next) {
-  res.send('respond with a resource');
+router.get('/:id', async function (req, res, next) {
+  const id = req.params.id;
+  const auction = await Auctions.findByPk(id);
+  res.json(auction).end();
 });
 
-router.post('/', function (req, res, next) {
-  res.send('respond with a resource');
+router.post('/', async function (req, res, next) {
+  const auction = req.body;
+  await Auctions.create(auction);
+  res.status(201).json(auction).end();
 });
 
 router.patch('/:id', function (req, res, next) {
