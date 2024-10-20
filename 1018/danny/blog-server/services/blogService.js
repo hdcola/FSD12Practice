48bf0f289c0e2b6c8f5e61fe4c719fc543c7d081
hdcola <gh@hdcola.org>
2024-10-20 14:43:01 -0400

    add yup validate

--- 1018/danny/blog-server/services/blogService.js
+++ 1018/danny/blog-server/services/blogService.js
@@ -1,8 +1,20 @@
 const { Blog, User } = require('../models');
 const ApiError = require('../utils/api-error');
 const { Sequelize } = require('sequelize');
+const yup = require('yup');
+
+const createBlogSchema = yup.object().shape({
+  title: yup.string().required().min(10),
+  content: yup.string().required().min(20),
+  userId: yup.number().required(),
+});
 
 async function createBlog({ title, content, userId }) {
+  try {
+    await createBlogSchema.validate({ title, content, userId });
+  } catch (err) {
+    throw ApiError.badRequest('Validation Error', err.errors);
+  }
   try {
     const newBlog = await Blog.create({
       title,
