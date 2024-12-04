9e06d137ede8160c825b02b8a19fd80098c937bb
hdcola <gh@hdcola.org>
2024-12-04 08:57:38 -0500

    add order

--- 1202/danny/bakery/Pages/Order.cshtml.cs
+++ 1202/danny/bakery/Pages/Order.cshtml.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using bakery.Data;
 using bakery.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -14,6 +15,18 @@ public class OrderModel : PageModel
     [BindProperty(SupportsGet = true)]
     public int Id { get; set; }
     public Product Product { get; set; } = new();
+
+    [BindProperty, Range(1, int.MaxValue, ErrorMessage = "You must order at least one item")]
+    public int Quantity { get; set; } = 1;
+    [BindProperty, Required, EmailAddress, Display(Name = "Your Email Address")]
+    public string OrderEmail { get; set; } = "";
+    [BindProperty, Required, Display(Name = "Shipping Address")]
+    public string ShippingAddress { get; set; } = "";
+    [BindProperty]
+    public decimal UnitPrice { get; set; }
+    [TempData]
+    public string? Confirmation { get; set; }
+
     public async Task OnGetAsync()
     {
         var product = await context.Products.FindAsync(Id);
@@ -21,5 +34,19 @@ public class OrderModel : PageModel
         {
             Product = product;
         }
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        var product = await context.Products.FindAsync(Id);
+        if (product == null) return NotFound();
+        Product = product;
+        if (ModelState.IsValid)
+        {
+            Confirmation = @$"You have ordered {Quantity} x {Product.Name} 
+                        at a total cost of {Quantity * UnitPrice:c}";
+            return RedirectToPage("/OrderSuccess");
+        }
+        return Page();
     }
 }
