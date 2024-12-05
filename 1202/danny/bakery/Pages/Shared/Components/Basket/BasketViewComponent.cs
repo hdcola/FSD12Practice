8bd79ab0fe93149dc2dc52e4d42d77a3cf390f82
hdcola <gh@hdcola.org>
2024-12-04 19:58:20 -0500

    add cart component

---
+++ 1202/danny/bakery/Pages/Shared/Components/Basket/BasketViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace bakery.Pages.Shared.Components.Basket
+{
+    public class BasketViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            Models.Basket basket = new();
+            if (Request.Cookies[nameof(Basket)] is not null)
+            {
+                var basketJson = Request.Cookies[nameof(Basket)];
+                if (basketJson is not null)
+                {
+                    basket = JsonSerializer.Deserialize<Models.Basket>(basketJson) ?? new Models.Basket();
+                }
+            }
+            return View(basket);
+        }
+    }
+};
