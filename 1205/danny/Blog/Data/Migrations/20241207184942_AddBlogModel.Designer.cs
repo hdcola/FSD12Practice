a36a979336f6ece1793eebc8fb4e33941afbee90
hdcola <gh@hdcola.org>
2024-12-07 16:04:40 -0500

    add blog model

--- 1205/danny/Blog/Data/Migrations/20241207184942_AddBlogModel.Designer.cs
+++ 1205/danny/Blog/Data/Migrations/20241207184942_AddBlogModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Blog.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Blog.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241207184942_AddBlogModel")]
+    partial class AddBlogModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.11");
